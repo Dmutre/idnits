0d28b31e4e30f4463a781016976c972a03d4c017
Vladimir <walker7732@gmail.com>
2025-02-13 12:55:09 +0200

    feat: added has-more-than-15-pages-and-not-table-of-contents

--- tests/fixtures/base-doc.mjs
+++ tests/fixtures/base-doc.mjs
@@ -40,6 +40,7 @@ export const baseTXTDoc = {
       bracketedRfcReferences: []
     },
     possibleIssues: {
+      isTableOfContentsExists: null,
       updatesRfcWithLetter: [],
       obsoletesWithLetter: [],
       isAbstractNumbered: null,
